ea376fd491e0d700c432c16b81704ddbb9df2d64
lekhacanhdai <khacdai0801@gmail.com>
2021-11-18 21:02:54 +0700

    ket thuc khoa kien thuc co ban --> Mo hinh MVC

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const handlebars = require("express-handlebars");
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 const port = 3000
 
@@ -14,8 +18,22 @@ var hbs = handlebars.create({
 app.engine('html', hbs.engine);
 app.set('view engine', 'html');
 app.set('views', './src/resources/views')
-app.use(morgan('combined'))
+
+// app.use(morgan('combined'))
+
 app.get("/", (req, res) => res.render("home"));
 app.get("/news", (req, res) => res.render("news"));
+
+app.get("/search", (req, res) => {
+    console.log(req.query.q === "");
+    res.render("search");
+});
+
+app.post("/search", (req, res) => {
+    // console.log(req.query.q);
+    console.log(req.body);
+    res.send("");
+});
+
 
 app.listen(port, () => console.log(`App dang chay o localhost:${port}`));
