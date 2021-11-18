7044fd794a6e32c0a42f68b31de227777af6ccfc
lekhacanhdai <khacdai0801@gmail.com>
2021-11-18 11:53:09 +0700

    use scss express-handlebars static file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+const path = require("path")
 const express = require("express");
 const morgan = require('morgan');
 const handlebars = require("express-handlebars");
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 const port = 3000
 
-var hbs = handlebars.create();
-app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+var hbs = handlebars.create({
+    extname : ".html"
+});
+app.engine('html', hbs.engine);
+app.set('view engine', 'html');
+app.set('views', './src/resources/views')
 app.use(morgan('combined'))
-app.get("/", (req, res) => res.send(`<h1 style="color:red;" >Hello world</h1>`));
+app.get("/", (req, res) => res.render("home"));
+app.get("/news", (req, res) => res.render("news"));
 
 app.listen(port, () => console.log(`App dang chay o localhost:${port}`));
